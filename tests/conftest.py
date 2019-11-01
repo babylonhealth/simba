6ae15343a7d6b38e72537170c0434971934bdf14
April Shen <april.shen@babylonhealth.com>
2019-11-01 16:51:18 +0000

    linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 
+
 INPUTS_ = [
     np.eye(3),
     np.eye(3, 2),
@@ -8,8 +9,13 @@ INPUTS_ = [
     np.zeros((3, 2)),
     np.ones((3, 2)),
     np.ones((3, 3)),
-    np.matrix([np.linspace(1, 5, 5), np.linspace(2, -4, 5), np.linspace(-10, -15, 5)])
+    np.matrix([
+        np.linspace(1, 5, 5),
+        np.linspace(2, -4, 5),
+        np.linspace(-10, -15, 5)
+    ])
 ]
+
 
 @pytest.fixture(autouse=True)
 def random_seed():
--- tests/utils/test_embedding.py
+++ tests/utils/test_embedding.py
@@ -12,10 +12,15 @@ FREQ_PATH = "tests/fixtures/test_freq.txt"
 @pytest.mark.parametrize(
     'test_pairs',
     [
-        ([['Hakuna', 'Matata']], (['Hakuna', 'Matata'], {'Hakuna': 0, 'Matata': 1})),
+        ([['Hakuna', 'Matata']],
+         (['Hakuna', 'Matata'], {'Hakuna': 0, 'Matata': 1})),
         ([], ([], {})),
-        ([['Hakuna', 'Matata'], ['What', 'a', 'wonderful', 'phrase'], ['Hakuna', 'Matata']],
-         (['Hakuna', 'Matata', 'What', 'a', 'wonderful', 'phrase'], {'Hakuna': 0, 'Matata': 1, 'What': 2, 'a': 3, 'wonderful': 4, 'phrase': 5})),
+        ([['Hakuna', 'Matata'],
+          ['What', 'a', 'wonderful', 'phrase'],
+          ['Hakuna', 'Matata']],
+         (['Hakuna', 'Matata', 'What', 'a', 'wonderful', 'phrase'],
+          {'Hakuna': 0, 'Matata': 1, 'What': 2, 'a': 3,
+           'wonderful': 4, 'phrase': 5})),
     ]
 )
 def test_create_dictionary(test_pairs):
